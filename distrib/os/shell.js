90129a56202ee885bc3f83541251a6aa6d39ae30
Kyle <16782191+kylecourounis@users.noreply.github.com>
2023-08-29 12:37:25 +0000

    Create date/time command (#3)

--- distrib/os/shell.js
+++ distrib/os/shell.js
@@ -45,6 +45,9 @@ var TSOS;
             // prompt <string>
             sc = new TSOS.ShellCommand(this.shellPrompt, "prompt", "<string> - Sets the prompt.");
             this.commandList[this.commandList.length] = sc;
+            // date
+            sc = new TSOS.ShellCommand(this.shellDate, "date", "Displays the current date and time in the shell.");
+            this.commandList[this.commandList.length] = sc;
             // ps  - list the running processes and their IDs
             // kill <id> - kills the specified process id.
             // Display the initial prompt.
@@ -243,6 +246,10 @@ var TSOS;
             else {
                 _StdOut.putText("Usage: prompt <string>  Please supply a string.");
             }
+        }
+        shellDate(args) {
+            let date = new Date();
+            _StdOut.putText(date.toLocaleString());
         }
     }
     TSOS.Shell = Shell;
--- source/os/shell.ts
+++ source/os/shell.ts
@@ -73,6 +73,13 @@ module TSOS {
                                   "<string> - Sets the prompt.");
             this.commandList[this.commandList.length] = sc;
 
+
+            // date
+            sc = new ShellCommand(this.shellDate,
+                                  "date",
+                                  "Displays the current date and time in the shell.");
+            this.commandList[this.commandList.length] = sc;
+
             // ps  - list the running processes and their IDs
             // kill <id> - kills the specified process id.
 
@@ -282,6 +289,12 @@ module TSOS {
             } else {
                 _StdOut.putText("Usage: prompt <string>  Please supply a string.");
             }
+        }
+
+        public shellDate(args: string[]) {
+            let date = new Date();
+
+            _StdOut.putText(date.toLocaleString());
         }
 
     }
