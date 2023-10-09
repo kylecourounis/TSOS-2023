b8e83cddea703f6e3527c63420b6196405966bee
Kyle <16782191+kylecourounis@users.noreply.github.com>
2023-10-09 23:19:00 +0000

    Some notes

--- source/os/memoryManager.ts
+++ source/os/memoryManager.ts
@@ -1,5 +1,7 @@
 module TSOS {
     export class MemoryManager {
-        
+        // TODO for Project 3
+
+        // Lay out the functions for creating and managing the three segments of memory
     }
 }
